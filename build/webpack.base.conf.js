11f7fe39b4fd96a1e3030283b1dccb97f21ea651
songhejia <851232853@qq.com>
2017-07-07 13:32:04 +0800

    webpack优化

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -6,7 +6,7 @@ var vueLoaderConfig = require('./vue-loader.conf')
 
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const extractSass = new ExtractTextPlugin({
   filename: "[name].[contenthash].css",
@@ -96,38 +96,6 @@ module.exports = {
           fallback: "style-loader"
         })
       },
-      // // the url-loader uses DataUrls.
-      // // the file-loader emits files.
-      // {
-      //   test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      //   // Limiting the size of the woff fonts breaks font-awesome ONLY for the extract text plugin
-      //   // loader: "url?limit=10000"
-      //   use: "url-loader"
-      // },
-      // {
-      //   test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-      //   use: 'file-loader'
-      // }
-
-      // {
-      //   test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-      //   use: {
-      //     loader: 'url-loader',
-      //     options: {
-      //       limit: 10000,
-      //       name: 'assets/fonts/[name].[ext]'
-      //     }
-      //   }
-      // }, {
-      //   test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-      //   use: {
-      //     loader: 'url-loader',
-      //     options: {
-      //       limit: 10000,
-      //       name: 'assets/images/[name].[ext]'
-      //     }
-      //   }
-      // },
     ]
   },
   plugins: [
